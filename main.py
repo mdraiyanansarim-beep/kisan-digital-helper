2ad2517cba2dc4b6d850f34de82815c32c940f3b
mdraiyanansarim-beep <mdraiyanansarim@gmail.com>
2026-07-06 19:19:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ st.title("किसान साथी")
 # स्वागत संदेश
 welcome_text = "नमस्ते! मैं आपका डिजिटल सहायक हूँ। मैं आपकी खेती से जुड़ी जानकारी में मदद कर सकता हूँ।"
 
-# ऑटो-प्ले के लिए स्क्रिप्ट
-js_code = f"""
-<script>
-    var msg = new SpeechSynthesisUtterance("{welcome_text}");
-    msg.lang = 'hi-IN';
-    window.speechSynthesis.speak(msg);
-</script>
-"""
-
-# HTML के ज़रिए आवाज़ चलाएं
-st.components.v1.html(js_code, height=0)
+# बटन ताकि यूजर क्लिक करके आवाज़ सुन सके
+if st.button("आवाज़ सुनने के लिए यहाँ दबाएं"):
+    js_code = f"""
+    <script>
+        var msg = new SpeechSynthesisUtterance("{welcome_text}");
+        msg.lang = 'hi-IN';
+        window.speechSynthesis.speak(msg);
+    </script>
+    """
+    st.components.v1.html(js_code, height=0)
 
 st.write("अपनी खेती की जानकारी के लिए यहाँ पूछें।")
+
 
 
